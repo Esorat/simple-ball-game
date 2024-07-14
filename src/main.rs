91af92cf3e998ae6721bdc9d367d4aadd77ebea7
Esorat <kriptatrade@gmail.com>
2024-07-14 17:05:10 +0700

    add confine player movement. Screen limit.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
+const PLAYER_SIZE: f32 = 64.0; // this player sprite size.
+pub const PLAYER_SPEED: f32 = 500.0; // this player speed.
+
 fn main() {
     App::new().add_plugins(DefaultPlugins)
     .add_startup_system(spawn_player)
     .add_startup_system(spawn_camera)
     .add_system(player_movement)
+    .add_system(confine_player_movement)
     .run();
 }
 
@@ -42,7 +46,6 @@ pub fn spawn_camera(mut commands: Commands,window_query: Query<&Window, With<Pri
         });
 }
 
-pub const PLAYER_SPEED: f32 = 500.0;
 
 pub fn player_movement (
     keyboard_input: Res<Input<KeyCode>>,
@@ -70,5 +73,39 @@ pub fn player_movement (
     }
         
     transform.translation += direction * PLAYER_SPEED *time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+
+) {
+    if let Ok(mut player_transform) =  player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size: f32 = PLAYER_SIZE / 2.0; //32.0
+        let x_min: f32 = 0.0 + half_player_size;
+        let x_max: f32 = window.width() - half_player_size;
+        let y_min: f32 = 0.0 + half_player_size;
+        let y_max: f32 = window.height() - half_player_size; 
+
+        let mut translation = player_transform.translation; 
+
+        //Bound player X position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max; 
+        }
+
+        //Bound player Y position
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max; 
+        }
+
+        player_transform.translation = translation;
     }
 }
