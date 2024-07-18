cbb45b75c174a0cf33e0212b1786427a6192c374
Esorat <kriptatrade@gmail.com>
2024-07-18 23:53:19 +0700

    add spawn enemy timer and spawn enemy fn

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ pub const ENEMY_SIZE: f32 = 64.0; //enemy size.
 pub const NUMBER_OF_STARS: usize = 10; //number spawn stars.
 pub const STAR_SIZE: f32 = 30.0; //star sprite size.
 pub const STAR_SPAWN_TIMER: f32 = 1.0; //spawn stars timer.
+pub const SPAWN_ENEMY_TIMER: f32 = 5.0; // spawn enemy timer.
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
+        .init_resource::<EnemySpawnTimer>()
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_enemies)
@@ -29,6 +32,8 @@ fn main() {
         .add_system(update_score)
         .add_system(tick_spawn_timer)
         .add_system(spawn_stars_over_time)
+        .add_system(tick_enemy_spawn_timer)
+        .add_system(spawn_enemies_over_time)
         .run();
 }
 
@@ -64,6 +69,20 @@ impl Default for StarSpawnTimer {
     fn default() -> StarSpawnTimer {
         StarSpawnTimer {
             timer: Timer::from_seconds(STAR_SPAWN_TIMER, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Resource)]
+
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for EnemySpawnTimer {
+    fn default() -> EnemySpawnTimer {
+        EnemySpawnTimer {
+            timer: Timer::from_seconds(SPAWN_ENEMY_TIMER, TimerMode::Repeating),
         }
     }
 }
@@ -298,7 +317,7 @@ pub fn enemy_hit_player(
             let enemy_radius = ENEMY_SIZE / 2.0;
             if distance < player_radius + enemy_radius {
                 println!("Enemy hit player! Game Over!");
-                let sound_effect = asset_server.load("assets/audio/explosionCrunch_000.ogg");
+                let sound_effect = asset_server.load("audio/explosionCrunch_000.ogg");
                 audio.play(sound_effect);
                 commands.entity(player_entity).despawn();
             }
@@ -339,8 +358,8 @@ pub fn update_score(score: Res<Score>) {
     }
 }
 
-pub fn tick_spawn_timer(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time> ) {
-star_spawn_timer.timer.tick(time.delta());
+pub fn tick_spawn_timer(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time>) {
+    star_spawn_timer.timer.tick(time.delta());
 }
 
 pub fn spawn_stars_over_time(
@@ -348,7 +367,6 @@ pub fn spawn_stars_over_time(
     window_query: Query<&Window, With<PrimaryWindow>>,
     asset_server: Res<AssetServer>,
     star_spawn_timer: ResMut<StarSpawnTimer>,
-
 ) {
     if star_spawn_timer.timer.finished() {
         let window = window_query.get_single().unwrap();
@@ -362,6 +380,34 @@ pub fn spawn_stars_over_time(
                 ..default()
             },
             Star {},
+        ));
+    }
+}
+
+pub fn tick_enemy_spawn_timer(mut enemy_spawn_timer: ResMut<EnemySpawnTimer>, time: Res<Time>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemies_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: Res<EnemySpawnTimer>,
+) {
+    if enemy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            },
         ));
     }
 }
